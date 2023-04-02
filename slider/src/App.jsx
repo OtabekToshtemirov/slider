df946c20db6ba2e0573f3a1f7b653de4fb95ab9a
OtabekToshtemirov <47772571+Otabek04@users.noreply.github.com>
2023-04-02 11:22:40 +0500

    completed

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -1,33 +1,105 @@
 import './App.css';
-import star from './assets/img_10.png';
-import arrow from './assets/arrow-up-right.svg';
+
+import star from "./assets/img_10.png";
+import arrow from "./assets/arrow-up-right.svg";
+import React from "react";
 
 function App() {
-   const carousel = document.querySelector('.cards');
 
-   let isDraggingStart = false, prevPage, scrollLeft;
+    const items = [
+        {
+            id: 1,
+            title: "Indebt",
+            img: "img_1",
+        },
+        {
+            id: 2,
+            title: "Fayz Baraka",
+            img: "img_2",
+        },
+        {
+            id: 3,
+            title: "Jizzax city",
+            img: "img_3",
+        },
+        {
+            id: 4,
+            title: "Ideal Cleaning",
+            img: "img_4",
+        },
+        {
+            id: 5,
+            title: "Lazydev",
+            img: "img_5",
+        },
+        {
+            id: 6,
+            title: "Mamirov Group",
+            img: "img_6",
+        },
+        {
+            id: 7,
+            title: "JS Ta'minot",
+            img: "img_7",
+        },
+        {
+            id: 8,
+            title: "InReport",
+            img: "img_8",
+        },
+        {
+            id: 9,
+            title: "Imba Studio",
+            img: "img_9",
+        }
+        ]
 
-   const dragStart = (e) => {
-        isDraggingStart = true;
-        prevPage = e.pageX;
-        scrollLeft = carousel.scrollLeft;
-   }
-   const dragging = (e) => {
-        if(!isDraggingStart) return;
-        e.preventDefault();
-        const x = e.pageX - prevPage;
-        carousel.scrollLeft = scrollLeft - x;
-   }
-    const dragEnd = () => {
-        isDraggingStart = false;
+  // infinity autoslide to left 340 px every 3 seconds
+    React.useEffect(() => {
+
+        const interval = setInterval(() => {
+            const cards = document.querySelector('.cards');
+            cards.style.transition = 'transform 0.5s ease-in-out';
+            setTimeout(() => {
+                cards.appendChild(cards.firstElementChild);
+                cards.style.transition = 'transform 0.5s ease-in-out';
+                cards.style.transform = 'translateX(0)';
+            }, 4000);
+        }, 4000);
+    }, []);
+
+    // drag cards
+    React.useEffect(() => {
+        const cards = document.querySelector('.cards');
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+        cards.addEventListener('mousedown', (e) => {
+            isDown = true;
+            cards.classList.add('active');
+            startX = e.pageX - cards.offsetLeft;
+            scrollLeft = cards.scrollLeft;
+        });
+        cards.addEventListener('mouseleave', () => {
+            isDown = false;
+            cards.classList.remove('active');
+        });
+        cards.addEventListener('mouseup', () => {
+            isDown = false;
+            cards.classList.remove('active');
+        });
+        cards.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - cards.offsetLeft;
+            const walk = (x - startX) * 3; //scroll-fast
+            cards.scrollLeft = scrollLeft - walk;
+        });
     }
+    , []);
 
 
 
-
-    carousel.addEventListener('mousedown', dragStart);
-    carousel.addEventListener('mousemove', dragging);
-    carousel.addEventListener('mouseup', dragEnd);
 
 
 
@@ -37,139 +109,26 @@ function App() {
         <div className="title">
             <span className=" text-white">Dotsoft -</span> <span className="blue_title ">things done</span>
         </div>
-            <div className="cards overflow-hidden">
-                <div className="card img_1 relative" >
+        <div className="cards overflow-hidden">
+            {items.map((item) => (
+                <div key={item.id} className={`card ${item.img} relative`}>
                     <div className="wrap">
-                        <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                        <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>Indebt</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-
-                </div>
-                <div className="card img_2 relative" >
-                    <div className="wrap">
-                        <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                        <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>Fayz Baraka</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-
-                </div>
-                <div className="card img_3 relative" >
-                    <div className="wrap">
-
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>Jizzax city</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
+                        <span className="border border-white-100 text-white bg-black p-1 px-2 rounded-full mt-2 ml-3 absolute">DEVELOPMENT</span>
+                        <span className=" flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
+                            <img className=" w-1/6 mt-1 inline-block mx-8" src={star} alt="star"/>
+                            <p>{item.title}</p>
+                            <a className="inline-block up " href="">
+                                <img src={arrow} alt="arrow"/>
+                            </a>
+                        </span>
                     </div>
                 </div>
-                <div className="card img_4 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
+            ))}
+        </div>
 
-                        <p>Ideal Cleaning</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
 
-                    </span>
-                    </div>
-                </div>
-                <div className="card img_5 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
 
-                        <p>Lazydev</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-                </div>
-                <div className="card img_6 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p className="mr-8">Mamirov Group</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-                </div>
-                <div className="card img_7 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>JS Ta'minot</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-                </div>
-                <div className="card img_8 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>InReport</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-                </div>
-                <div className="card img_9 relative" >
-                    <div className="wrap">
-                    <span className="text-white bg-black p-1 rounded-lg mt-2 ml-3 absolute">DEVELOPMENT</span>
-                    <span className="flex  bg-black text-white text-2xl absolute items-center w-full h-1/4 block bottom-0 rounded-b-3xl  ">
-                        <img className=" w-1/6 mt-2 inline-block mx-8" src={star} alt="star"/>
-
-                        <p>Imba studio</p>
-                        <a className="inline-block up " href="">
-                        <img src={arrow} alt="arrow"/>
-                        </a>
-
-                    </span>
-                    </div>
-                </div>
-
-            </div>
-    </div>
+     </div>
   )
 
 }
